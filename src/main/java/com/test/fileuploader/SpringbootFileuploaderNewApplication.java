a28c976140513d0e1a9af337f57481c73683b536
Sandaka <sandaka94@gmail.com>
2020-09-09 21:29:23 +0530

    first commit

---
+++ src/main/java/com/test/fileuploader/SpringbootFileuploaderNewApplication.java
@@ -0,0 +1,13 @@
+package com.test.fileuploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFileuploaderNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFileuploaderNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/fileuploader/SpringbootFileuploaderNewApplicationTests.java
@@ -0,0 +1,16 @@
+package com.test.fileuploader;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootFileuploaderNewApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
